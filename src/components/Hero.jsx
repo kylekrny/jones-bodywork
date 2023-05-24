9c686cd67e5fd74e115cf8b50ea703bd0741c04f
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-05-24 08:42:01 -0600

    update image sizing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import profile from '@/images/jeff.jpeg'
+import profileMobile from "@/images/jeff-square.jpeg"
 import Image from 'next/image'
 import { Header } from './Header'
 
@@ -22,15 +23,14 @@ export function Hero() {
 
             <div className="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
               <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
-                <div className="hidden sm:mb-10 sm:flex"></div>
+                <div className="hidden sm:mb-10 sm:flex">
+                </div>
                 <h1 className="text-4xl font-bold tracking-tight text-[#3F88C5] sm:text-6xl">
                   Massage therapist that listens to you.
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                  To me, bodywork is about listening to the body. Looking at and
-                  treating the body as a whole, not parts. I never work on the
-                  same body twice and understanding that allows me to tailor
-                  each massage to you in the moment.
+                  To me, bodywork is about listening to the body. Looking at and treating the body as a
+                  whole, not parts. I never work on the same body twice and understanding that allows me to tailor each massage to you in the moment.
                 </p>
                 <div className="mt-10 flex items-center gap-x-6">
                   {/* <a
@@ -53,7 +53,7 @@ export function Hero() {
         <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <Image
             className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full"
-            src={profile}
+            src={profileMobile}
             alt=""
           />
         </div>
