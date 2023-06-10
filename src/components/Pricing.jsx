a40fe7026ae440567cfefa42a095be9efe62caa2
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-06-10 16:35:57 -0600

    update pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -14,14 +14,13 @@ export function Pricing() {
             <h2 className="text-3xl font-bold tracking-tight text-[#3F88C5] sm:text-4xl">
               Fair and simple pricing
             </h2>
-            <p className="mt-4 text-lg leading-8 text-gray-600">
+            <p className="mt-4 text-lg leading-8 text-gray-600 w-2/3">
               The time you&apos;re paying for is actual massage time. When you
               pay for a hour long session, you will get an hour long session. I
-              believe in being fair. My prices are not going to make me rich,
-              and I am okay with that.
+              believe in being fair.
             </p>
           </div>
-          <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
+          <fdl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
             {stats.map((stat) => (
               <div key={stat.id} className="flex flex-col bg-gray-400/5 p-8">
                 <dt className="text-sm font-semibold leading-6 text-gray-600">
@@ -32,10 +31,10 @@ export function Pricing() {
                 </dd>
               </div>
             ))}
-          </dl>
+          </fdl>
           <div className="text-center">
             <p className="mt-8 text-lg font-bold leading-8 text-gray-600">
-              Actual table time. Tips not accepted.
+              TIme on the table. No table setup time. Tips not accepted.
             </p>
           </div>
         </div>
--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -1,61 +1,67 @@
-import { InboxIcon, TrashIcon, UsersIcon } from '@heroicons/react/24/outline'
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  LockClosedIcon,
+} from '@heroicons/react/20/solid'
 
 const features = [
   {
-    name: 'Unlimited inboxes',
+    name: 'Push to deploy',
     description:
-      'Non quo aperiam repellendus quas est est. Eos aut dolore aut ut sit nesciunt. Ex tempora quia. Sit nobis consequatur dolores incidunt.',
+      'Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu. Phasellus praesent ligula sit faucibus.',
     href: '#',
-    icon: InboxIcon,
+    icon: CloudArrowUpIcon,
   },
   {
-    name: 'Manage team members',
+    name: 'SSL certificates',
     description:
-      'Vero eum voluptatem aliquid nostrum voluptatem. Vitae esse natus. Earum nihil deserunt eos quasi cupiditate. A inventore et molestiae natus.',
+      'Pellentesque enim a commodo malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.',
     href: '#',
-    icon: UsersIcon,
+    icon: LockClosedIcon,
   },
   {
-    name: 'Spam report',
+    name: 'Simple queues',
     description:
-      'Et quod quaerat dolorem quaerat architecto aliquam accusantium. Ex adipisci et doloremque autem quia quam. Quis eos molestiae at iure impedit.',
+      'Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod ut condimentum.',
     href: '#',
-    icon: TrashIcon,
+    icon: ArrowPathIcon,
   },
 ]
 
-export function SecondaryFeatures() {
+export default function Example() {
   return (
-    <div className="bg-gray-900 py-24 sm:py-32">
+    <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Stay on top of customer support
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">
+            Deploy faster
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Everything you need to deploy your app
+          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
+            Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
+            In mi viverra elit nunc.
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
             {features.map((feature) => (
               <div key={feature.name} className="flex flex-col">
-                <dt className="text-base font-semibold leading-7 text-white">
-                  <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-500">
-                    <feature.icon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </div>
+                <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                  <feature.icon
+                    className="h-5 w-5 flex-none text-indigo-600"
+                    aria-hidden="true"
+                  />
                   {feature.name}
                 </dt>
-                <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-300">
+                <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
                   <p className="flex-auto">{feature.description}</p>
                   <p className="mt-6">
                     <a
                       href={feature.href}
-                      className="text-sm font-semibold leading-6 text-indigo-400"
+                      className="text-sm font-semibold leading-6 text-indigo-600"
                     >
                       Learn more <span aria-hidden="true">→</span>
                     </a>
