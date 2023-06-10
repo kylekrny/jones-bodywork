da6b3f5d83ede4a931279bf45e99ea620c4ae2d4
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-06-10 17:17:05 -0600

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export function Footer() {
   return (
     <footer className="bg-slate-50">
       <Container>
-        <div className="py-8">
+        <div className="pt-8">
           <div className="flex flex-col items-center">
             <Logo className="mx-auto h-10 w-auto" />
             <Button href="/contact" className="mt-6 bg-[#3F88C5]">
@@ -18,7 +18,7 @@ export function Footer() {
               </span>
             </Button>
           </div>
-          <nav className="mt-10 text-sm" aria-label="quick links">
+          <nav className="mt-8 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
               <NavLink href="/#about">About</NavLink>
               <NavLink href="/contact">Contact</NavLink>
@@ -28,8 +28,8 @@ export function Footer() {
             </div>
           </nav>
         </div>
-        <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
-          <div className="flex gap-x-6">
+        <div className="flex flex-col items-center border-t border-slate-400/10 py-10">
+          <div className="flex gap-x-8 mb-2">
             <Link
               href="mailto:jeff@jonesbodywork.com"
               className="group"
@@ -43,6 +43,13 @@ export function Footer() {
               aria-label="Jeff Jones phone number"
             >
               (720) 480-4979
+            </Link>
+            <Link
+              href="tel:7206757448"
+              className="group"
+              aria-label="Jeff Jones phone number"
+            >
+              7345 S. Pierce St, Suite 203C Littleton, CO 80128
             </Link>
           </div>
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
