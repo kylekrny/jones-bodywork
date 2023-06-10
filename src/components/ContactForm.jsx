e8acf7af534a82a540cf145b8733dae10eef0c46
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-06-10 16:54:43 -0600

    update contact section

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -61,20 +61,13 @@ const [message, setMessage] = useState(null)
           className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
           aria-hidden="true"
         >
-          <div
-            className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
         </div>
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Let&apos;s get down to business
+            Let&apos;s Chat
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Phone: 720-675-7448 | Email: hello@fixmyschitt.com
+            Phone: (720) 480-4979 | Email: jeff@jonesbodywork.com
           </p>
         </div>
         <form
@@ -161,24 +154,6 @@ const [message, setMessage] = useState(null)
                 Phone number
               </label>
               <div className="relative mt-2.5">
-                <div className="absolute inset-y-0 left-0 flex items-center">
-                  <label htmlFor="country" className="sr-only">
-                    Country
-                  </label>
-                  <select
-                    id="country"
-                    name="country"
-                    className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                  >
-                    <option>US</option>
-                    <option>CA</option>
-                    <option>EU</option>
-                  </select>
-                  <ChevronDownIcon
-                    className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </div>
                 <input
                   type="tel"
                   name="phone-number"
