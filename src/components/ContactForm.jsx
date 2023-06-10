b7dd58760d162ae5e9ea9e5ead5317e73cf9294e
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-06-10 16:57:43 -0600

    added office address

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -60,14 +60,19 @@ const [message, setMessage] = useState(null)
         <div
           className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
           aria-hidden="true"
-        >
-        </div>
+        ></div>
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Let&apos;s Chat
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
             Phone: (720) 480-4979 | Email: jeff@jonesbodywork.com
+          </p>
+          <p className="mt-1 text-lg leading-8 text-gray-600">
+            Office:{' '}
+            <a href="https://www.google.com/maps/place/7345+S+Pierce+St,+Littleton,+CO+80128/data=!4m2!3m1!1s0x876b7e60d663a621:0xfbcb410553da9129?sa=X&ved=2ahUKEwjyidjEppHyAhURH80KHQiICQ8Q8gF6BAgOEAE">
+              7345 S. Pierce St, Suite 203C Littleton, CO 80128
+            </a>
           </p>
         </div>
         <form
