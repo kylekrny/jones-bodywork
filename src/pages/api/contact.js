39b9bfa6d46820b8692a6523acee698a94db23e1
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-11-18 10:40:04 -0500

    update email

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -9,16 +9,16 @@ async function main(body) {
         secure: false,
         auth: {
             user: "fullofbulk@gmail.com",
-            pass: "zvujxrgfqvwndfbb"
+            pass: "lxcgqenbedqwevvu"
         },
     })
 
 
     await transporter.sendMail({
-        from: `"Jones Bodywork`,
-        to: "jeff@jonesbodywork.com",
+        from: `Jones Bodywork <noreply@jonesbodywork.com>`,
+        to: "kyledkearney@gmail.com",
         subject: "New Form Submission",
-        text: `name: ${body.first} ${body.last},\r message: ${body.message},\r phone: ${body.phone},\r company: ${body.company}, `,
+        text: `name: ${body.first} ${body.last},\r message: ${body.message},\r email: ${body.email}, \r phone: ${body.phone},\r`,
     })
 }
 
@@ -26,9 +26,10 @@ export default async function handler(req, res) {
     const body = req.body;
 
     main(body).then(() => {
-        res.status(200).json({title: "Thank you",  message: "I will reach out with in the next business day.", button: "Okay", code: 200 })
-    }).catch(() => {
-        res.status(400).json({title: "There seems to be an error",  message: "Looks like something went wrong. Please feel free to try that again or reach out to me directly.", button: "Okay", code: 400 })
+        res.status(200).json({ title: "Thank you", message: "I will reach out with in the next business day.", button: "Okay", code: 200 })
+    }).catch((error) => {
+        console.log(error)
+        res.status(400).json({ title: "There seems to be an error", message: "Looks like something went wrong. Please feel free to try that again or reach out to me directly.", button: "Okay", code: 400 })
     });
 
 }
